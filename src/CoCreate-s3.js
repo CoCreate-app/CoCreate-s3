fcf1ce68055f6b27298f58d94e3796f4475c63ed
frank pagan <frank@cocreate.app>
2021-01-18 22:25:35 +0000

    fix: action

--- src/CoCreate-s3.js
+++ src/CoCreate-s3.js
@@ -1,26 +1,36 @@
 const S3 = require('aws-sdk/clients/s3')
 const path = require('path');
 var fs = require('fs');
-module.exports = function(file) {
-  console.log("Saving File in S3");
-  let s3 = new S3({
-    accessKeyId: 'AKIAZKETJ5PS53VASL4A',
-    secretAccessKey: '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',
-  })
-  const config = {
-    Key: path.basename(file),
-    Bucket: 'testcrudbucket',
-    Body: fs.createReadStream(file),
-    ACL: 'public-read'
-  }
-  s3.upload(config, function(err, data) {
+const core = require('@actions/core');
+const AWS_ACCESS_KEY = core.getInput('AWS_ACCESS_KEY', {
+  required: true
+});
+const AWS_KEY_ID = core.getInput('AWS_KEY_ID', {
+  required: true
+});
+const DIST_FILE_NAME = core.getInput('DIST_FILE_NAME', {
+  required: true
+});
 
-    if (err) {
-      console.log("error happend:", err)
-      process.exit(1)
-    }
-    else
-      console.log("File saved in S3", data)
 
-  })
+console.log("Saving File in S3");
+let s3 = new S3({
+  accessKeyId: AWS_KEY_ID,
+  secretAccessKey: AWS_ACCESS_KEY,
+})
+const config = {
+  Key: path.basename(DIST_FILE_NAME),
+  Bucket: 'testcrudbucket',
+  Body: fs.createReadStream(DIST_FILE_NAME),
+  ACL: 'public-read'
 }
+s3.upload(config, function(err, data) {
+
+  if (err) {
+    console.log("could not upload to s3: ", err)
+    process.exit(1)
+  }
+  else
+    console.log("File saved in S3", data)
+
+})
