720d63dc63b1b178de1403e8d654ce11d1e42138
alipour <hossein.4lipour@gmail.com>
2021-07-03 20:47:37 +0000

    fix: aa

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# echo "yarn install start"
-# yarn install
-# echo "yarn install end"
+echo "yarn install start"
+yarn install
+echo "yarn install end"
 echo "executing aws upload start"
 export NODE_ENV=production
 nodejs ./src/index.js
